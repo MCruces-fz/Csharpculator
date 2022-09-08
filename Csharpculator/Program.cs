c598f6d0f82cd04d564b01e4da60e9dbd4dc8f90
MCruces-fz <mcsquared.fz@gmail.com>
2022-09-08 11:12:45 +0200

    First approach to very simple calculator.

--- Csharpculator/Program.cs
+++ Csharpculator/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double num1 = 0; double num2 = 0;
+
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+Console.WriteLine("Type a number, and the press Enter");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Type another number, and the press Enter");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Choose an option from the followinf list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Substract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
+
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+
+Console.Write("Press any key to close the Csharpculator console app...");
+Console.ReadKey();
